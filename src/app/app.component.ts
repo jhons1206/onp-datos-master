5c4902d6991078eedec35169728e9a379d4f3a31
Jhon Sandoval <webdeveloper84@gmail.com>
2021-02-16 09:55:31 -0500

    Ruteo Inicio component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.router.navigate(['/Afp']);
+    this.router.navigate(['/Inicio']);
   }
 
 }
